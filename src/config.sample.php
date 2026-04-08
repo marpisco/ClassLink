96066fe4bca7194872f4b71019f76f4c4f190677
Marco Pisco <marco@marcopisco.com>
2026-04-08 10:52:43 +0100

    feat(config sample): update the sample configuration file
    remove english comments and put portuguese comments only. make the comments more clear. add a little bit of more information regarding the stuff that is in the configuration
    
    add reference to the future configuration that will be created in the docs file.

--- src/config.sample.php
+++ src/config.sample.php
@@ -1,16 +1,30 @@
 <?php
     require_once(__DIR__ . '/../vendor/autoload.php');
     use League\OAuth2\Client\Provider\GenericProvider;
+    /* Ficheiro de configuração
+       Mais informação sobre o ficheiro de configuração está na documentação.
+    */
 
-    // Email configuration
+    // Configuração da Base de Dados
+    $db = array(
+        'tipo' => 'mysql',         // Apenas há suporte a MYSQL (mysql = mariadb) por enquanto
+        'servidor' => 'localhost',
+        'user' => 'reservasalas',
+        'password' => 'salaspass', 
+        'db' => 'reservasalas',
+        'porta' => 3306
+    );
+        
+    // Configuração do servidor de email
     $mail = array(
         'ativado' => true,
         'servidor' => 'smtp.gmail.com',
         'porta' => 465,
         'autenticacao' => true,
-        // caso a autenticação seja por starttls, usar PHPMailer::ENCRYPTION_STARTTLS
-        // caso a autenticação seja por ssl, usar PHPMailer::ENCRYPTION_SMTPS
-        // caso não seja necessário autenticação, por false na opção autenticacao, e não importar-se para os outros
+        // Tipo de Segurança: Explicação:
+        /// caso a autenticação seja por starttls, usar PHPMailer::ENCRYPTION_STARTTLS
+        /// caso a autenticação seja por ssl, usar PHPMailer::ENCRYPTION_SMTPS
+        /// caso não seja necessário autenticação, por false na opção autenticacao, e não importar-se para os outros
         'tipodeseguranca' => 'PHPMailer::ENCRYPTION_STARTTLS ou PHPMailer::ENCRYPTION_SMTPS',
         'username' => '',
         'fromname' => 'Reserva de Salas',
@@ -18,25 +32,13 @@
         'password' => ''
     );
 
-    // Database configuration (MySQL/MariaDB)
-    // SECURITY: Use strong passwords and restrict database user permissions
-    $db = array(
-        'tipo' => 'mysql',
-        'servidor' => 'localhost',
-        'user' => 'reservasalas',
-        'password' => 'salaspass',  // CHANGE THIS to a strong password
-        'db' => 'reservasalas',
-        'porta' => 3306
-    );
-
-    // OAuth 2.0 configuration
-    // SECURITY: Keep clientId and clientSecret confidential
+    // Configuração do Fornecedor de Autenticação (OAuth 2.0)
     $provider = new GenericProvider([
         'urlAuthorize'            => 'https://authentik.devenv.marcopisco.com/application/o/authorize/',
         'urlAccessToken'          => 'https://authentik.devenv.marcopisco.com/application/o/token/',
         'urlResourceOwnerDetails' => 'https://authentik.devenv.marcopisco.com/application/o/userinfo/',
-        'clientId'     => 'clientid',  // CHANGE THIS
-        'clientSecret' => 'clientsecret',  // CHANGE THIS and keep it secret
+        'clientId'     => 'clientid',
+        'clientSecret' => 'clientsecret',
         'redirectUri'  => 'https://' . $_SERVER['HTTP_HOST'] . '/login'
     ]);
 ?>
