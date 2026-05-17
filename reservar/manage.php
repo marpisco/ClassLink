0cdae619340c517533ec8916da907b98d17d8bc3
Marco Pisco <marco@marcopisco.com>
2026-05-17 23:16:48 +0000

    reservar/manage.php: include func/get_config.php to provide get_app_config()

--- reservar/manage.php
+++ reservar/manage.php
@@ -1,6 +1,7 @@
 <?php
 require_once(__DIR__ . '/../func/logaction.php');
 require_once(__DIR__ . '/../func/email_helper.php');
+require_once(__DIR__ . '/../func/get_config.php');
 require_once(__DIR__ . '/../src/db.php');
 if (session_status() === PHP_SESSION_NONE) { session_start(); }
 if (!isset($_SESSION['validity']) || $_SESSION['validity'] < time()) {
